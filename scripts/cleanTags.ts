677f852d7df33ef28e0c085b220a717944bc87bf
Zolyn <zorin1477@gmail.com>
2022-07-05 15:06:42 +0800

    chore: update script

--- scripts/cleanTags.ts
+++ scripts/cleanTags.ts
@@ -1,15 +1,10 @@
 // Delete tags that are not created by Zolyn
 import { bold, green, red, white } from 'kolorist'
-import { version } from '../package.json'
-import { getGitTags } from '../src'
 import { execCommand } from '../src/utils'
 
 async function main() {
-  const tags = (await getGitTags()).reverse()
+  const tags = await execCommand('git', ['tag', '--no-merged', 'HEAD']).then(r => r.split('\n'))
   for (const tag of tags) {
-    if (tag === `v${version}`)
-      break
-
     await execCommand('git', ['tag', '-d', tag])
     console.log(`${green(bold('Success:'))} delete tag ${white(tag)}`)
   }
