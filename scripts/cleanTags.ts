9bfafeda4d011cabbdf6c794330b88b9f2d534cd
Zolyn <zorin1477@gmail.com>
2022-07-17 19:29:33 +0800

    chore: update script

--- scripts/cleanTags.ts
+++ scripts/cleanTags.ts
@@ -1,13 +1,20 @@
 // Delete tags that are not created by Zolyn
+import { p } from '@antfu/utils'
 import { bold, green, red, white } from 'kolorist'
+import { filterTagsCreatedByRepo, getGitTags, getUpstreamRepo, isCommitAheadOfTargetCommit } from '../src'
 import { execCommand } from '../src/utils'
 
 async function main() {
-  const tags = await execCommand('git', ['tag', '--no-merged', 'HEAD']).then(r => r.split('\n'))
-  for (const tag of tags) {
-    await execCommand('git', ['tag', '-d', tag])
-    console.log(`${green(bold('Success:'))} delete tag ${white(tag)}`)
-  }
+  const tags = await getGitTags()
+  const upstreamInfo = await getUpstreamRepo()
+  const savedTag = (await filterTagsCreatedByRepo(tags, upstreamInfo))[0]
+
+  await p(tags).forEach(async (tag) => {
+    if (!(await isCommitAheadOfTargetCommit(tag, `upstream/${upstreamInfo.defaultBranch!}`)) && tag !== savedTag) {
+      await execCommand('git', ['tag', '-d', tag])
+      console.log(`${green(bold('Success:'))} delete tag ${white(tag)}`)
+    }
+  })
 }
 
 main().catch((e) => {
