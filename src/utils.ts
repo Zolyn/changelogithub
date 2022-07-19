b37ae473e7a2e41dfd3997384fe086ae64d2a0cc
Zolyn <zorin1477@gmail.com>
2022-07-19 20:42:32 +0800

    chore: separate dependencies of `remark` for customization

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,16 @@
+import { unified } from 'unified'
+import remarkParse from 'remark-parse'
+import remarkStringify from 'remark-stringify'
+
 export async function execCommand(cmd: string, args: string[]) {
   const { execa } = await import('execa')
   const res = await execa(cmd, args)
   return res.stdout.trim()
+}
+
+/**
+ * Forked from https://github.com/remarkjs/remark/blob/main/packages/remark/index.js
+*/
+export function createRemark() {
+  return unified().use(remarkParse).use(remarkStringify, { bullet: '-', listItemIndent: 'one' }).freeze()
 }
